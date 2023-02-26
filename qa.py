ce10056a77eed727df06f6c29c8c7a9423bab736
Lilly Chen <lillychen@Lillys-MacBook-Air.local>
2023-02-25 19:45:05 -0700

    remove flask, trying streamlit

--- qa.py
+++ qa.py
@@ -6,30 +6,23 @@ from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
 import argparse
 import os
-from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def query_bot():
-  parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
-  parser.add_argument('question', type=str, help='The question to ask the notion DB')
-  args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
+parser.add_argument('question', type=str, help='The question to ask the notion DB')
+args = parser.parse_args()
 
 # Load the LangChain.
-  index = faiss.read_index("docs.index")
+index = faiss.read_index("docs.index")
 
-  with open("faiss_store.pkl", "rb") as f:
-      store = pickle.load(f)
+with open("faiss_store.pkl", "rb") as f:
+  store = pickle.load(f)
 
-  store.index = index
-  chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
-  result = chain({"question": args.question})
+store.index = index
+chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+result = chain({"question": args.question})
 
-  print(f"Answer: {result['answer']}")
-  print(f"Sources: {result['sources']}")
+print(f"Answer: {result['answer']}")
+print(f"Sources: {result['sources']}")
 
   return("Success")
-
-if __name__ == "__qa__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
